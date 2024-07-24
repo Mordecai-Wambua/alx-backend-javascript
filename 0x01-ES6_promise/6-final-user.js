d9f75c6933a804ce459c8eb30677452b4d6424d6
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-07-24 19:45:48 +0000

    Task 06: Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const photo = uploadPhoto(fileName);
+  const sign = signUpUser(firstName, lastName);
+
+  return Promise.allSettled([photo, sign])
+    .then((result) => {
+      result.map((item) => ({
+        status: item.status,
+        value: item.status === 'fulfilled' ? item.value : String(item.reason),
+      }));
+    });
+}
